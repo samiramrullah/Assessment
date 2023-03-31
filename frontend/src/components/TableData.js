d485ac9597204b6e5473d88968a2045ffe96d353
samiramrullah <samiramrullah@gmail.com>
2023-04-01 01:21:14 +0530

    table added

--- frontend/src/components/TableData.js
+++ frontend/src/components/TableData.js
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 const TableData = () => {
-    
+    const [allData,setAllData]=useState();
+    useEffect(()=>{
+       axios.get('http://localhost:5000/user')
+       .then((res)=>setAllData(res.data?.doc))
+       .catch(err=>console.log(err))
+    },[])
   const data = [
     {
       name: "Peter Hernandez",
@@ -17,6 +22,9 @@ const TableData = () => {
     },
     
   ]
+  console.log('====================================');
+  console.log(allData);
+  console.log('====================================');
     return (
         <div className="  w-full max-w-7xl overflow-x-auto">
             <table className=" min-w-full rounded-md border border-gray-200 overflow-hidden">
@@ -27,11 +35,11 @@ const TableData = () => {
                         {/* ::Name */}
                         <th className="py-3 px-4 text-sm font-medium uppercase tracking-wide" scope="col">Name</th>
                         {/* ::Job Title */}
-                        <th className="py-3 px-4 text-sm font-medium uppercase tracking-wide" scope="col">Job Title</th>
+                        {/* <th className="py-3 px-4 text-sm font-medium uppercase tracking-wide" scope="col">Job Title</th> */}
                         {/* ::Email */}
                         <th className="py-3 px-4 text-sm font-medium uppercase tracking-wide" scope="col">Email</th>
                         {/* ::Created Date */}
-                        <th className="py-3 px-4 text-sm font-medium uppercase tracking-wide" scope="col">Created</th>
+                        {/* <th className="py-3 px-4 text-sm font-medium uppercase tracking-wide" scope="col">Created</th> */}
                         {/* ::Actions */}
                         <th className="py-3 px-4 text-center text-sm font-medium uppercase tracking-wide" scope="col">Actions</th>
                     </tr>
@@ -40,22 +48,22 @@ const TableData = () => {
 
                 {/* :TABLE BODY */}
                 <tbody className="">
-                    {data?.map((user, index) => (
+                    {allData?.map((user, index) => (
                         <tr key={user.name} className={`${index % 2 === 0 ? "bg-gray-50" : "bg-gray-100"} whitespace-nowrap`}>
                             {/* ::User Name */}
                             <td className="py-3 px-4 text-base text-gray-700 font-semibold">{user.name}</td>
                             {/* ::User Job Title */}
-                            <td className="py-3 px-4 text-base text-gray-500 font-medium">{user.job}</td>
+                            {/* <td className="py-3 px-4 text-base text-gray-500 font-medium">{user.job}</td> */}
                             {/* ::User Email */}
                             <td className="py-3 px-4 text-base text-gray-500 font-medium">{user.email}</td>
                             {/* ::User Created Date */}
-                            <td className="py-3 px-4 text-base text-gray-500 font-medium">{user.created}</td>
+                            {/* <td className="py-3 px-4 text-base text-gray-500 font-medium">{user.created}</td> */}
                             {/* ::Action Buttons */}
                             <td className="py-3 px-4 flex justify-around items-center space-x-6 text-base text-gray-700 font-medium">
                                 {/* :::edit button */}
-                                <button type="button" className="text-sm text-gray-700 font-semibold hover:underline hover:text-black">Edit</button>
+                                {/* <button type="button" className="text-sm text-gray-700 font-semibold hover:underline hover:text-black">Edit</button> */}
                                 {/* :::delete button */}
-                                <button type="button" className="text-sm text-red-500 font-semibold hover:text-red-600">Delete</button>
+                                <button type="button" className="text-sm text-black font-semibold hover:text-blue-500">Reply</button>
                             </td>
                         </tr>
                     ))
