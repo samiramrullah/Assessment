f35766fcf5b37dd3a678b7578ba001cd7587ebe7
samiramrullah <samiramrullah@gmail.com>
2023-04-01 06:31:19 +0530

    logo removed

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -10,11 +10,7 @@ const Dashboard = () => {
                     class="fixed top-0 z-10 ml-[-100%] flex h-screen w-full flex-col justify-between border-r bg-white px-6 pb-3 transition duration-300 md:w-4/12 lg:ml-0 lg:w-[25%] xl:w-[20%] 2xl:w-[15%] dark:bg-gray-800 dark:border-gray-700"
                 >
                     <div>
-                        <div class="-mx-6 px-6 py-4">
-                            <a href="1" title="home">
-                                <img src="images/logo.svg" class="w-32" alt="tailus logo" />
-                            </a>
-                        </div>
+                        
 
                         <div class="mt-8 text-center">
                             <img
